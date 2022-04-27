60d2b134ed1214a4ec068f47fee5646dc5d0ae2a
Samir Boumendjel <73393090+Miroo1010@users.noreply.github.com>
2022-04-27 13:18:38 +0000

    working on elif and bolding sections of string

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-
+bmi=round((float(weight) / float(height)** 2))
+if bmi <= 18.5:
+  print(f"Your BMI is {int(bmi)}, you are \033[1m underweight.\033[0m")
+  
+print(f"Your BMI is {int(bmi)}")
 
 
 
